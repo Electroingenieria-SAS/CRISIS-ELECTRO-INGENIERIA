3f05c9e62eda6595dd0f6cdb90be59b152d4bee1
JEPTAC <j.perez@ei.com.co>
2026-09-14 15:37:11 -0500

    rebuild(v8): integrate staff character system into world runtime

--- src/v8/World.ts
+++ src/v8/World.ts
@@ -1,1 +1,21 @@
-export { World } from './WorldCore';
+import * as THREE from 'three';
+import { World as WorldCore } from './WorldCore';
+import { StaffDirector } from './visual/StaffDirector';
+
+export class World extends WorldCore {
+  private readonly staff = new StaffDirector();
+
+  constructor(scene: THREE.Scene) {
+    super(scene);
+  }
+
+  override init(): void {
+    super.init();
+    this.staff.rebuild(this.group);
+  }
+
+  override update(dt: number): void {
+    super.update(dt);
+    this.staff.update(dt);
+  }
+}
